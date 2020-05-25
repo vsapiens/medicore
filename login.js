08f2da9631e3bc6409d391cfbe2d99bd656b4af6
vsapiens <erickfrank@icloud.com>
2020-05-25 16:41:30 -0500

    Added tecnología

---
+++ login.js
@@ -0,0 +1,36 @@
+function validateForm() {
+  let form = document.getElementById("login-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+  if (username.value === "") {
+    alert("Proporcionar el nombre del usuario");
+    return false;
+  }
+  if (password.value === "") {
+    alert("Password incorrecto");
+    return false;
+  }
+  if (username.value[0] === "M") {
+    window.open("farmaciaHome.html");
+  } else {
+    window.open("doctorHome.html");
+  }
+}
+
+function watchForm() {
+  let form = document.getElementById("login-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    validateForm();
+  });
+}
+
+function init() {
+  watchForm();
+}
+
+init();
